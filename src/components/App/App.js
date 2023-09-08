59bb84b7de0d79eaa3afe34f47a71ce311fbc0cd
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-09-08 17:19:34 +0300

    Fixed mistakes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Notification } from '../Notification/Notification';
 import { Container } from './App.styled';
 import { Section } from 'components/Section/Section';
+
 export class App extends Component {
   state = {
     good: 0,
@@ -12,32 +13,34 @@ export class App extends Component {
   };
 
   handleButtonClick = feedbackType => {
-    const lowerCaseFeedbackType = feedbackType.toLowerCase();
+    // const lowerCaseFeedbackType = feedbackType.toLowerCase();
     this.setState(prevState => ({
-      [lowerCaseFeedbackType]: prevState[lowerCaseFeedbackType] + 1,
+      [feedbackType]: prevState[feedbackType] + 1,
     }));
   };
 
   countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+    const feedbackValues = Object.values(this.state);
+    const total = feedbackValues.reduce((acc, value) => acc + value, 0);
+    return total;
   };
 
   countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
     return total === 0 ? 0 : Math.round((good / total) * 100);
   };
 
   render() {
     const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
+    const optionsKeys = Object.keys(this.state);
     const positivePercentage = this.countPositiveFeedbackPercentage();
     return (
       <Container>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['Good', 'Neutral', 'Bad']}
+            options={optionsKeys}
             onLeaveFeedback={this.handleButtonClick}
           />
         </Section>
